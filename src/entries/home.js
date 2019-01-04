f9240b53ccf07ebdf52651fdb038954233acd713
Juan Manuel Lopez <juanlg@me.com>
2019-01-03 23:54:33 -0700

    Configuring redux with react

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
+import { Provider } from 'react-redux';
 // console.log('Hola mundo!' )
+
+import { createStore } from 'redux';
+
+const initialState = {
+    data: {
+        ...data
+    }
+}
+
+const store = createStore(
+    (state) => state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState())
 
 const homeContainer = document.getElementById('home-container')
 
-// ReactDOM.render(que voy a renderizar, donde lo haré);
-// const holaMundo = <h1>hola Estudiante!</h1>;
-hydrate( <Home data={data} />, homeContainer);
+
+
+render(
+    <Provider store = {store}>
+        <Home />
+    </Provider>
+    , homeContainer);
 
